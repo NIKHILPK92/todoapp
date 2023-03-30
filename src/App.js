bf78ba2877ad5d4c257a4664b15a4a390b5fa21e
Nikhil P K <33482945+NIKHILPK92@users.noreply.github.com>
2023-03-30 22:46:03 +0530

    counter and deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from "react"
 import About from "./Component/About/About"
 import Header from "./Component/Header/Header"
 import Todo from "./Component/Todo/Todo"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom"
 import Footer from "./Component/Footer/Footer"
 
 class App extends Component {
@@ -11,8 +16,9 @@ class App extends Component {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" exact Component={Todo} />
-          <Route path="/about" Component={About} />
+          <Route exact path="/todoapp" component={Todo} />
+          <Route path="/todoapp/about" component={About} />
+          <Redirect exact from="/" to="/todoapp" />
         </Routes>
         <Footer />
       </Router>
